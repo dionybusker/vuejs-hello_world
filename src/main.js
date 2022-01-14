829015e306f45dc7da66f582b240259d143996f2
Diony Busker <dionybusker@gmail.com>
2022-01-14 10:13:31 +0100

    Deleted comments

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
 import { createApp } from 'vue';
-// import * as Vue from 'vue';
 import App from './App.vue';
-// import moment from 'moment';
-
-// Vue.moment = moment;
 
 createApp(App).mount('#app');
